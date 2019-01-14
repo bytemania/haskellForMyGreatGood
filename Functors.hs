63bfc540b4d46ac8ef7c5d0b1b427f3c9715cb8d
silvaa1 <antonio.silva@paddypowerbetfair.com>
2019-01-14 21:43:25 +0000

    functors

---
+++ Functors.hs
@@ -0,0 +1,74 @@
+module Functors where
+
+import Data.Char
+import Data.List
+import Control.Applicative
+import Data.Tuple
+
+main1 :: IO ()
+main1 = do
+    line <- getLine
+    let line' = reverse line
+    putStrLn $ "You said " ++ line' ++ " backwards!"
+    putStrLn $ "Yes, you really said " ++ line' ++ " backwards!"
+
+main2 :: IO ()
+main2 = do
+    line <- fmap reverse getLine
+    putStrLn $ "You said " ++ line ++ " backwards!"
+    putStrLn $ "Yes, you really said " ++ line ++ " backwards!"
+
+main3 :: IO ()
+main3 = do
+    line <- fmap (intersperse '-' . reverse . map toUpper) getLine
+    putStrLn line
+
+data CMaybe a = CNothing | CJust Int a deriving Show
+
+instance Functor CMaybe where
+    fmap f CNothing = CNothing
+    fmap f (CJust counter x) = CJust (counter + 1) (f x)
+
+myAction' :: IO String
+myAction' = do
+    a <- getLine
+    b <- getLine
+    return $ a ++ b
+
+myAction :: IO String
+myAction = (++) <$> getLine <*> getLine
+
+main4 :: IO ()
+main4 = do
+    a <- (++) <$> getLine <*> getLine
+    putStrLn $ "The two lines concatenated turn out to be: " ++ a
+
+sequenceA' :: Applicative f => [f a] -> f [a]
+sequenceA' [] = pure []
+sequenceA' (h:t) = (:) <$> h <*> sequenceA' t
+
+sequenceA'' :: (Applicative f, Show a) => [f a] -> f [a]
+sequenceA'' = foldr (liftA2 (:)) (pure [])
+
+data Profession = Fighter | Archer | Accountant
+data Race = Human | Elf | Orc | Goblin
+data PlayerCharacter = PlayerCharacter Race Profession
+
+newtype CharList = CharList { getCharList :: [Char] } deriving (Eq, Show)
+
+newtype Pair b a = Pair { getPair :: (a,b) }
+
+instance Functor (Pair c) where
+    fmap f (Pair (x, y)) = Pair (f x, y)
+
+data CoolBool' = CoolBool' {getCoolBool' :: Bool}
+
+helloMe' :: CoolBool' -> String
+helloMe' (CoolBool' _) = "hello"
+
+newtype CoolBool = CoolBool {getCoolBool :: Bool}
+
+helloMe  :: CoolBool -> String
+helloMe (CoolBool _) = "hello"
+
+
